149777fa4b2e3367bb98b59f2d360f9e2b94a230
BrunoCzeck <brunoczeck@hotmail.com>
2025-03-14 22:48:13 -0300

    remove files

--- prisma/migrations/20240519050829_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `dbo.contrato` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `dbo.contrato` DROP COLUMN `status`;
